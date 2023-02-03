3d50140cb3dd2568667964f5ac059240d4a8841a
alviny <alviny11t@gmail.com>
2023-02-03 22:54:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "index"
+
+
+@app.route('/about')
+def about():
+    return render_template("site_back.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
